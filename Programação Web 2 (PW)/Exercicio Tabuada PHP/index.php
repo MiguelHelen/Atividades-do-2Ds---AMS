48121e9cd7c1dbb2e5fdbf597fdfeb6d25f61359
Miguel Heleno <miguelheleno468@gmail.com>
2025-02-27 10:01:06 -0300

    Create index.php

---
+++ Programação Web 2 (PW)/Exercicio Tabuada PHP/index.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "Tabuada do $i:<br>";
+    for ($m = 1; $m <= 10; $m++) {
+        echo "$i * $m= " . ($i * $m) . 
+        "<br>";
+    }
+    echo "<br>"; 
+}
+
+
+
+?>
